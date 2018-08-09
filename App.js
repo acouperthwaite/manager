af27440e89c99954edab5013a4af11bde8bff4e8
Andrew Couperthwaite <acouperthwaite@gmail.com>
2018-08-08 19:54:48 -0600

    Added common components, added AuthReducer.js with initial state, added reducers index, added actions index

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import firebase from 'firebase';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm'
+
+class App extends Component {
+  componentWillMount(){
+    const config = {
+      apiKey: "AIzaSyCW3T1sNUYrF-Tz_cehJuSaMkHjvL3kgO0",
+      authDomain: "manager-af9f8.firebaseapp.com",
+      databaseURL: "https://manager-af9f8.firebaseio.com",
+      projectId: "manager-af9f8",
+      storageBucket: "",
+      messagingSenderId: "513436285899"
+    };
+    firebase.initializeApp(config);
+  }
+  render(){
+    return(
+      <Provider store={createStore(reducers)}>
+        <View>
+          <LoginForm />
+        </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
